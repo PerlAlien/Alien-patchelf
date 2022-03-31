70ce9d1cff57c460e1f4facf1351186d6fecb605
shawnlaffan <shawnlaffan@gmail.com>
2022-03-31 17:25:38 +1100

    Document patchelf function.

--- lib/Alien/patchelf.pm
+++ lib/Alien/patchelf.pm
@@ -36,9 +36,21 @@ Alien::patchelf - Alien module for the patchelf utility
 
 Sometimes you need to change the rpath of binary files,
 for example for aliens that are installed in non-standard locations.  
-The patchelf utility is used
+The patchelf utility is used for this purpose.
 
 This is not useful on Windows since dynamic libs are loaded from the PATH.
+
+As with mosst aliens, if your system provides the patchelf utility
+then that will be used.  If not then it will be downloaded and installed for you.  
+
+=head1 METHODS
+
+This moule inherits from Alien:::Base so all of those methods are available.
+
+=head2 patchelf
+
+Runs the patchelf command, passing any arguments through.
+Uses a L<system> call via L<Capture::Tiny>, and it is up to the caller to handle the results.  
 
 
 =head1 REPORTING BUGS
